c571bc0652aaba7bddf282fe27d72cb08319ad13
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-03-23 19:35:11 +0100

    connected ui to python

---
+++ index.py
@@ -0,0 +1,22 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+import sys
+
+
+ui,_ = loadUiType('library.ui')
+
+class MainApp(QMainWindow , ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
